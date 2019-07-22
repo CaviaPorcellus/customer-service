e7fbb9afa49fe351763d0a30d07738a695338971
I337593 <kevin.jin01@sap.com>
2019-07-22 16:55:13 +0800

    Implement customized circuit breaker using AOP

--- src/main/java/com/example/waiterservice/CustomerServiceApplication.java
+++ src/main/java/com/example/waiterservice/CustomerServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAspectJAutoProxy
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/waiterservice/support/CircuitBreakerAspect.java
@@ -0,0 +1,58 @@
+package com.example.waiterservice.support;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Component
+@Aspect
+public class CircuitBreakerAspect {
+
+  private static final int FAIL_THRESHOLD = 2;
+  private static final int BREAKER_THRESHOLD = 2;
+
+  private Map<String, AtomicInteger> failCounter = new ConcurrentHashMap<>();
+  private Map<String, AtomicInteger> breakerCounter = new ConcurrentHashMap<>();
+
+  @Around("execution(* com.example.waiterservice.integration..*(..))")
+  public Object doWithCircuitBreaker(ProceedingJoinPoint pjp) throws Throwable {
+    String sig = pjp.getSignature().getName();
+
+    Object res;
+    try {
+      if (!failCounter.containsKey(sig)) {
+        failCounter.put(sig, new AtomicInteger(0));
+      }
+      if (!breakerCounter.containsKey(sig)) {
+        breakerCounter.put(sig, new AtomicInteger(0));
+      }
+
+      // check if failure number larger than threshold
+      if (failCounter.get(sig).get() > FAIL_THRESHOLD) {
+        // circuit break
+        if (breakerCounter.get(sig).get() <= BREAKER_THRESHOLD) {
+          int breakTimes = breakerCounter.get(sig).incrementAndGet();
+          log.info("Circuit break return null, break {} times", breakTimes);
+          return null;
+        }
+      }
+
+      res = pjp.proceed();
+      failCounter.get(sig).set(0);
+      breakerCounter.get(sig).set(0);
+    } catch (Throwable t) {
+      int failureTimes = failCounter.get(sig).incrementAndGet();
+      log.error("Mehod call failure {} times, error message {}", failureTimes, t.getMessage());
+      breakerCounter.get(sig).set(0);
+      throw t;
+    }
+    return res;
+  }
+}
